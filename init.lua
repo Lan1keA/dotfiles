de49da7f98f505cbb861e2ce7b9331c3c605b9c9
dev2ero <dev2ero@foxmail.com>
2022-08-22 18:01:43 +0800

    update

--- init.lua
+++ init.lua
@@ -2,17 +2,18 @@
 vim.o.nu=true
 vim.o.rnu=true
 vim.o.noshowmode=true
-vim.o.expandtab=true
+vim.o.expandtab=false
 vim.o.cursorline=true
 vim.o.cursorcolumn=true
-vim.o.tabstop=4
-vim.o.shiftwidth=4
+vim.o.tabstop=2
+vim.o.shiftwidth=2
 vim.o.encoding='UTF-8'
 vim.o.fileencodings='UTF-8,GBK,GB2312'
 
 
 -- Options --
 vim.cmd([[ colorscheme edge ]])
+vim.api.nvim_set_option("clipboard","unnamed")
 -- vim.api.nvim_set_hl(0, 'Normal', {bg="#000000"})
 
 
